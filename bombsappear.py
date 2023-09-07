6ce3e74ac9d121017c5001b6852b220727d1cbb4
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-07 22:50:54 +0200

    création du fichier bombsappear.py

--- bombsappear.py
+++ bombsappear.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class ItemsAppear(pygame.sprite.Sprite):
+class BombsAppear(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.images = {'bombgreen': [], 'bombice': [], 'bombred': [], 'bombsound': []}
@@ -12,37 +12,8 @@ class ItemsAppear(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.apple_images = {'applegolden': [], 'applenormal': [], 'applerotten': []}
-        self.apple_type = 'applegolden'
-        self.apple_image_index = 0
-        self.apple_image = self.apple_images[self.apple_type][self.apple_image_index]
-        self.load_images()
 
     def load_images(self):
-        apple_golden = pygame.image.load("pixelart/apples_pack/apple_golden.png")
-        rect_applegolden = [(0, 0, 30, 30)]
-        image_applegolden = pygame.Surface(rect_applegolden[2:], pygame.SRCALPHA).convert()
-        image_applegolden.blit(apple_golden, (0, 0), rect_applegolden)
-        image_applegolden = pygame.transform.scale(image_applegolden, (30, 30))
-        self.apple_images['applegolden'].append(image_applegolden)
-
-        apple_normal = pygame.image.load("pixelart/apples_pack/apple_regular.png")
-        rect_applenormal = [(0, 0, 30, 30)]
-        image = pygame.Surface(rect_applenormal[2:], pygame.SRCALPHA).convert()
-        image.blit(apple_normal, (0, 0), rect_applenormal)
-        image = pygame.transform.scale(image, (30, 30))
-        self.apple_images['applenormal'].append(image)
-
-        apple_rotten = pygame.image.load("pixelart/apples_pack/apple_rotten.png")
-        self.rect_applerotten = [
-            (0, 0, 30, 30)
-        ]
-        for rect in self.rect_applerotten:
-            image = pygame.Surface(rect[2:], pygame.SRCALPHA).convert()
-            image.blit(apple_rotten, (0, 0), rect)
-            image = pygame.transform.scale(image, (30, 30))
-            self.apple_images['applerotten'].append(image)
-
         spritesheet_bombgreen = pygame.image.load("pixelart/bombs/bombgreen/idel.png")
         self.image_rects_bombgreen = [
             (10, 20, 30, 30),
@@ -109,10 +80,4 @@ class ItemsAppear(pygame.sprite.Sprite):
     def draw_bombs(self, fenetre):
         fenetre.blit(self.image, (self.rect.x, self.rect.y))
 
-    def change_apple_type(self, apple_type):
-        self.apple_type = apple_type
-        self.apple_image_index = 0
-        self.apple_image = self.apple_images[self.apple_type][self.apple_image_index]
 
-    def draw_apple(self, fenetre):
-        fenetre.blit(self.apple_image, (self.rect.x, self.rect.y))
