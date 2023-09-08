e7fa9ae93d0dffb3ce1a2f9c1e1c73f8eec4b685
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-08 20:27:27 +0200

    Mise à jour de items.py

--- items.py
+++ items.py
@@ -1,6 +1,7 @@
 import pygame
 
-class ItemsAppear:
+
+class Items:
     def __init__(self, x, y):
         self.images = {'applegolden': [], 'applenormal': [], 'applerotten': []}
         self.load_images()
