840b341d16f951ff27e39b86876f4b4a6efddf6d
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-11 21:59:33 +0200

    Mise à jour de game.py

--- game.py
+++ game.py
@@ -6,14 +6,13 @@ from items import Items
 
 from itemsappear import Itemsappear
 
+from snakesegment import SnakeSegment
+
 class GameScreen:
     def __init__(self, fenetre):
         self.fenetre = fenetre
         self.screen_color = (0, 0, 0)
         self.tool_color = (255, 255, 255)
-        self.snake_length = 90
-        self.snake_x = 10
-        self.snake_y = 20
         self.snake_speed_x = 1
         self.snake_speed_y = 1
         self.direction_x = 0
@@ -21,6 +20,11 @@ class GameScreen:
         self.snake_segments = []
         self.create_snake()
         self.clock = pygame.time.Clock()
+        self.snake_height = 15
+        self.snake_width = 60
+        self.is_game_over = False
+        self.game_over_displayed = False
+
 
         self.bombgreen = Bombs(20, 90)
         self.bombgreen.change_bomb_type('bombgreen')
@@ -32,19 +36,37 @@ class GameScreen:
         self.applegolden = Items(50, 220)
         self.applegolden.change_apple_type('applegolden')
 
-        self.items = Itemsappear(150, 300)
+        self.items = Itemsappear(200, 190)
 
     def create_snake(self):
-        self.snake_segments.append(pygame.Rect(self.snake_x, self.snake_y, self.snake_length, 30))
+        self.snake_segments = [SnakeSegment(10, 20, 1, 0), SnakeSegment(0, 20, 1, 0)]
+
 
     def game_over(self):
+        # Charger le fond d'écran
         self.background_gameover = pygame.image.load("image/game_over.jpg")
         self.background_gameover = pygame.transform.scale(self.background_gameover, (500, 700))
 
+        # Effacer l'écran
+        self.fenetre.fill(self.screen_color)
+
+        # Afficher le fond d'écran
+        self.fenetre.blit(self.background_gameover, (0, 0))
+
         # Ajouter un titre
         title_font = pygame.font.Font("font/Acme-Regular.ttf", 38)
-        title_text = title_font.render("Game Over", True, pygame.Color("gold"))
-        title_position = (self.fenetre.get_width() // 2, 50)
+        title_text = title_font.render("Game Over", True, pygame.Color("#a92c11"))
+        title_frame = pygame.Rect(230, 90, 210, 65)
+        pygame.draw.rect(self.fenetre, pygame.Color("black"), title_frame)
+        pygame.draw.rect(self.fenetre, pygame.Color("#a92c11"), title_frame, 3)
+
+        sentence_text = title_font.render("Tu es devenu un joli sac à main !!", True, pygame.Color("#FFC300"))
+        sentence_position = (10, 630)
+        sentence_title_frame = pygame.Rect(0, 620, 500, 65)
+        pygame.draw.rect(self.fenetre, pygame.Color("black"), sentence_title_frame)
+        pygame.draw.rect(self.fenetre, pygame.Color("#FFC300"), sentence_title_frame, 3)
+        title_position = (self.fenetre.get_width() // 2, 100)
+        self.fenetre.blit(sentence_text, sentence_position)
         self.fenetre.blit(title_text, title_position)
 
         pygame.display.update()
@@ -52,67 +74,82 @@ class GameScreen:
     def run_game(self):
         running = True
         FPS = 80
+        game_over_displayed = False  # Variable pour indiquer si l'écran de fin de jeu a été affiché
+        FPS = 80
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Déplacement du serpent
-            keys = pygame.key.get_pressed()
+            if not self.is_game_over:
 
-            if keys[K_UP] and self.direction_y != 1:
-                self.direction_x = 0
-                self.direction_y = -1
+                # Déplacement du serpent
+                keys = pygame.key.get_pressed()
 
-            if keys[K_DOWN] and self.direction_y != -1:
-                self.direction_x = 0
-                self.direction_y = 1
+                if keys[K_UP] and self.direction_y != 1:
+                    self.direction_x = 0
+                    self.direction_y = -1
 
-            if keys[K_RIGHT] and self.direction_x != -1:
-                self.direction_x = 1
-                self.direction_y = 0
+                if keys[K_DOWN] and self.direction_y != -1:
+                    self.direction_x = 0
+                    self.direction_y = 1
 
-            if keys[K_LEFT] and self.direction_x != 1:
-                self.direction_x = -1
-                self.direction_y = 0
+                if keys[K_RIGHT] and self.direction_x != -1:
+                    self.direction_x = 1
+                    self.direction_y = 0
 
-            for i in range(len(self.snake_segments) - 1, 0, -1):
-                self.snake_segments[i] = self.snake_segments[i - 1].copy()
+                if keys[K_LEFT] and self.direction_x != 1:
+                    self.direction_x = -1
+                    self.direction_y = 0
 
-            self.snake_segments[0].x += self.direction_x * self.snake_speed_x
-            self.snake_segments[0].y += self.direction_y * self.snake_speed_y
+                for i in range(len(self.snake_segments) - 1, 0, -1):
+                    self.snake_segments[i] = self.snake_segments[i - 1].direction_x
+                    self.snake_segments[i] = self.snake_segments[i - 1].direction_y
 
-            # Check for game over conditions
-            if (
-                    self.snake_segments[0].y < 0
-                    or self.snake_segments[0].y > 700
-                    or self.snake_segments[0].x < 0
-                    or self.snake_segments[0].x > 500
-            ):
-                self.game_over()
-                print("Game Over")
+                self.snake_segments[0].direction_x = self.direction_x
+                self.snake_segments[0].direction_y = self.direction_y
+
+                # Mise à jour des
+                if (
+                        self.snake_segments[0].y < 0
+                        or self.snake_segments[0].y + self.snake_height >= 700
+                        or self.snake_segments[0].x < 0
+                        or self.snake_segments[0].x + self.snake_width >= 500
+                        ):
+
+                    self.is_game_over = True
+                    print("Game Over")
 
 
-            # Clear the screen
-            self.fenetre.fill(self.screen_color)
+                # Effacer l'écran
+                self.fenetre.fill(self.screen_color)
 
-            # Draw each segment of the snake
-            for segment in self.snake_segments:
-                pygame.draw.rect(self.fenetre, self.tool_color, segment)
 
-            # Mettre à jour l'animation de la bombe bombgreen
-            if self.animation_counter % self.animation_speed == 0:
-                self.bombgreen.change_image()
-                self.bombred.change_image()
-            self.animation_counter += 1
+                self.items.update()
 
-            self.bombgreen.draw_bombs(self.fenetre)
-            self.bombred.draw_bombs(self.fenetre)
+                # Dessiner chaque segment du serpent
+                for segment in self.snake_segments:
+                    pygame.draw.rect(self.fenetre, self.tool_color, segment)
 
-            self.applegolden.draw_apple(self.fenetre)
-            self.apple_type.draw(self.fenetre)
+                # Mettre à jour l'animation de la bombe bombgreen
+                if self.animation_counter % self.animation_speed == 0:
+                    self.bombgreen.change_image()
+                    self.bombred.change_image()
+                self.animation_counter += 1
+
+                self.bombgreen.draw_bombs(self.fenetre)
+                self.bombred.draw_bombs(self.fenetre)
+
+                self.applegolden.draw_apple(self.fenetre)
+            else:
+                if not game_over_displayed:
+                    self.game_over()
+                    game_over_displayed = True
+                    pygame.display.flip()
+
 
             pygame.display.flip()
+
             # Control the FPS based on the desired speed
             self.clock.tick(FPS)
 
